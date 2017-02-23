ef79d60d08df9527c172b2fb90fe4c14919402a9
Salman <Salman@IMPC>
2017-02-23 05:40:25 +0000

    Initial Version

---
+++ SampaleAuthentication/Controllers/AccountController.cs
@@ -0,0 +1,137 @@
+﻿
+using System.Web.Mvc;
+using SampaleAuthentication.Extensions;
+using Microsoft.AspNet.Identity;
+using SampaleAuthentication.Models;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security;
+using System.Web;
+
+namespace SampaleAuthentication.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController :Controller
+    {
+
+        private readonly UserManager<AppUser> userManager;
+
+        public AccountController()
+            : this (StartUp.UserManagerFactory.Invoke())
+        {
+        }
+
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpGet]
+        public ActionResult LogIn(string returnUrl)
+        {
+            var model = new LogInModel
+            {
+                ReturnUrl = returnUrl
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> LogIn(LogInModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var user = await userManager.FindAsync(model.Email, model.Password);
+
+            if (user != null)
+            {
+                await SignIn(user);
+                return Redirect(GetRedirectUrl(model.ReturnUrl));
+            }
+
+            // user authN failed
+            ModelState.AddModelError("", "Invalid email or password");
+            return View();
+        }
+
+        public ActionResult LogOut()
+        {
+            GetAuthenticationManager().SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+            return RedirectToAction("index", "home");
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(RegisterModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var user = new AppUser
+            {
+                UserName = model.Email,
+                Country = model.Country,
+                Age = model.Age
+            };
+
+            var result = await userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                await SignIn(user);
+                return RedirectToAction("index", "home");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+
+            return View();
+        }
+
+        private string GetRedirectUrl(string returnUrl)
+        {
+            if (string.IsNullOrEmpty(returnUrl) || !Url.IsLocalUrl(returnUrl))
+            {
+                return Url.Action("index", "home");
+            }
+
+            return returnUrl;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && userManager != null)
+            {
+                userManager.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private async Task SignIn(AppUser user)
+        {
+            var identity = await userManager.CreateIdentityAsync(
+                user, DefaultAuthenticationTypes.ApplicationCookie);
+
+            GetAuthenticationManager().SignIn(identity);
+        }
+
+        private IAuthenticationManager GetAuthenticationManager()
+        {
+            var ctx = Request.GetOwinContext();
+            return ctx.Authentication;
+        }
+
+    }
+}
---
+++ SampaleAuthentication/Extensions/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace SampaleAuthentication.Extensions
+{
+    public class AppUser : IdentityUser
+    {
+        public string Country { get; set; }
+
+        public int Age { get; set; }
+    }
+}
